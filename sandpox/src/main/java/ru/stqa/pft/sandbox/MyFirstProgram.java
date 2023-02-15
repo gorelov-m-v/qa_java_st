00c967028cde5ab6b8139680aa71a4c97b6db0a1
gorelov-m-v <123184098+gorelov-m-v@users.noreply.github.com>
2023-02-15 19:28:27 +0300

    Добавил в класс метод main класса MyFirstProgram вывод ответа домашнего задания №2 и класс Point

--- sandpox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandpox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,7 +8,11 @@ public class MyFirstProgram {
         System.out.println("Площадь квадрата со стороной " + s.l + " = " +s.area());
 
         Rectangle r = new Rectangle(4, 6);
-        System.out.println("прямоугольника со сторонами " + r.a + " + " + r.b + " = " +r.area());
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " + " + r.b + " = " +r.area());
+
+        // Задание №2
+        Point p = new Point(1,1,4,5);
+        System.out.println("Расстояние между двумя точками на плоскости: A(" + p.ax + ", " + p.ay + ") и B(" + p.bx + ", " + p.by + ") равно: " + p.distance());
     }
     public static void hello(String name) {
         System.out.println("Hello, " + name + "!");
---
+++ sandpox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,24 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+    // Координаты точки A(x, y) на координатной плоскости.
+    public int ax;
+    public int ay;
+
+    // Координаты точки B(x, y) на координатной плоскости.
+    public int bx;
+    public int by;
+
+    public Point(int ax, int ay, int bx, int by) {
+        this.ax = ax;
+        this.ay = ay;
+        this.bx = bx;
+        this.by = by;
+    }
+
+    // Расстояние между двумя точками нахожу по формуле: distance = √((xb - xa)^2 + (yb - ya)^2)
+    public double distance() {
+        return  Math.sqrt(Math.pow((bx - ax), 2) + Math.pow((by - ay), 2));
+    }
+
+}
