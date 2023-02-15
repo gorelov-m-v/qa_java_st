c98fa2fd6e58502401af906dc24842a52cbd9f5a
gorelov-m-v <123184098+gorelov-m-v@users.noreply.github.com>
2023-02-15 19:00:13 +0300

    Превратил функции в методы

--- sandpox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandpox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,25 +4,13 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
         hello("Михаил");
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " +s.area());
 
-        double n = 5;
-        System.out.println("Площадь квадрата со стороной " + n + " = " +area(n));
-
-        double a = 4;
-        double b = 6;
-        System.out.println("прямоугольника со сторонами " + a + " + " + b + " = " +area(a, b));
-
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("прямоугольника со сторонами " + r.a + " + " + r.b + " = " +r.area());
     }
-
     public static void hello(String name) {
         System.out.println("Hello, " + name + "!");
-    }
-
-    public static double area(double l) {
-        return l * l;
-    }
-
-    public static double area(double a, double b) {
-        return a * b;
     }
 }
