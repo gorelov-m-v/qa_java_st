3fde1f5a8d9540c15673a6ecf1ef6db9f794ebb6
gorelov-m-v <123184098+gorelov-m-v@users.noreply.github.com>
2023-02-18 20:24:17 +0300

    Внес исправления в задание №3 исходя из замечаний к предыдущей задаче.

--- sandpox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandpox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -7,17 +7,20 @@ public class PointTests {
 
     @Test
     public void testPointsInDifferentQuarters() {
-        Point p = new Point(-1, -1, 4,5);
-        Assert.assertEquals(p.distance(), 7.810249675906654);
+        Point a = new Point(-1, -1);
+        Point b = new Point(4, 5);
+        Assert.assertEquals(a.distance(b), 7.810249675906654);
     }
     @Test
     public void testPointsInSameQuarter() {
-        Point p = new Point(1, 1, 4,5);
-        Assert.assertEquals(p.distance(), 5.0);
+        Point a = new Point(1, 1);
+        Point b = new Point(4, 5);
+        Assert.assertEquals(a.distance(b), 5.0);
     }
     @Test
     public void testPointsInSamePlace() {
-        Point p = new Point(1, 1, 1,1);
-        Assert.assertEquals(p.distance(), 0.0);
+        Point a = new Point(1, 1);
+        Point b = new Point(1, 1);
+        Assert.assertEquals(a.distance(b), 0.0);
     }
 }
