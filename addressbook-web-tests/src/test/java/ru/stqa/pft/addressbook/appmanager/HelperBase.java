663fd39244a41483190c1a3baa57674e1c193da4
gorelov-m-v <123184098+gorelov-m-v@users.noreply.github.com>
2023-02-20 12:50:50 +0300

    Перенес isAlertPresent isElementPresent из ApplicationManager в HelperBase.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -1,6 +1,8 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HelperBase {
@@ -17,5 +19,23 @@ public class HelperBase {
     protected void type(By locator, String text) {
         wd.findElement(locator).clear();
         wd.findElement(locator).sendKeys(text);
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            wd.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
     }
 }
