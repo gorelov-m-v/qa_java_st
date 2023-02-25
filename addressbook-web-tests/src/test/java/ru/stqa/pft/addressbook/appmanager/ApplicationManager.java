205b368926e0f7b48ce67da0f4b578dc53788513
gorelov-m-v <123184098+gorelov-m-v@users.noreply.github.com>
2023-02-25 23:47:39 +0300

    Рефакторинг. Убрал лишние импорты. Подсветил ДЗ.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,10 +1,8 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.*;
 
 
@@ -29,20 +27,20 @@ public class ApplicationManager {
         wd.switchTo().alert().accept();
     }
 
-        public void init() {
-            if (browser.equals(BrowserType.CHROME)) {
-                wd = new ChromeDriver();
-            } else if (browser.equals(BrowserType.FIREFOX)) {
-                wd = new FirefoxDriver();
-            }
-            wd.get("http://localhost/addressbook/group.php");
-            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-            groupHelper = new GroupHelper(wd);
-            contactHelper = new ContactHelper(wd);
-            navigationHelper = new NavigationHelper(wd);
-            sessionHelper = new SessionHelper(wd);
-            sessionHelper.login("admin", "secret");
+    public void init() {
+        if (browser.equals(BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
         }
+        wd.get("http://localhost/addressbook/group.php");
+        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        groupHelper = new GroupHelper(wd);
+        contactHelper = new ContactHelper(wd);
+        navigationHelper = new NavigationHelper(wd);
+        sessionHelper = new SessionHelper(wd);
+        sessionHelper.login("admin", "secret");
+    }
 
     public void stop() {
         wd.quit();
