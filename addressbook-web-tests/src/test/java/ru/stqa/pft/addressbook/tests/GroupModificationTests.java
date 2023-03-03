0d5c0c8ad11cf09ca1b23a0f8bccc812fb87014a
gorelov-m-v <123184098+gorelov-m-v@users.noreply.github.com>
2023-03-03 15:39:31 +0300

    Добавил возможность выбирать случайную группу в тестах.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -13,7 +13,7 @@ public class GroupModificationTests extends TestBase {
         if(! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test11", "test11", "test11"));
         }
-        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().selectGroup(before - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test22", "test22", "test22"));
         app.getGroupHelper().submitGroupModification();
