64d233edaa135489ef2f3adf7ec0ef8187a1f393
gorelov-m-v <123184098+gorelov-m-v@users.noreply.github.com>
2023-03-04 17:45:55 +0300

    Добавил возможность сравнивать множества в тесте создания контактов 2.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,6 +3,9 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
@@ -10,12 +13,23 @@ public class ContactCreationTests extends TestBase {
     @Test
     public void testContactCreationTests() {
         List<ContactData> before = app.getContactHelper().getContactList();
-        app.getContactHelper().createContact(new ContactData("Mikhail", "Junior",
-                                                             "Gorelov", "Junior",
-                                                             "89037776767", "testmail@testmail.com",
-                                                             "[none]"));
+        ContactData contact = new ContactData("Mikhail", "Junior",
+                                              "Gorelov", "Junior",
+                                              "89037776767", "testmail@testmail.com",
+                                              "[none]");
+        app.getContactHelper().createContact(contact);
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
+
+        before.add(contact);
+        int max = 0;
+        for(ContactData c : after) {
+            if(c.getId() > max) {
+                max = c.getId();
+            }
+        }
+        contact.setId(max);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 
 }
